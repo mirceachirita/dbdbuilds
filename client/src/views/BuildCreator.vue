fd38bdf5351d28148d65d21cdbde898dc63e025b
mirceachirita <mirceamihaiwdb@gmail.com>
2020-11-25 00:33:58 +0200

    Inventory perk with dummy data

---
+++ client/src/views/BuildCreator.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="builder-page page-content">
+    <div class="builder-optional-items">
+      <div class="builder-items-addons">
+        Item + Add-ons
+      </div>
+      <div class="builder-offering">
+        Offering
+      </div>
+    </div>
+    <div class="builder-perks">
+      Selected perks
+    </div>
+    <div class="builder-inventory-perks">
+      <div class="inventory-row first-row even">
+        <div class="inventory-perk has-perk-populated">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/left_behind.png" />
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk has-perk-populated">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empathy.png" />
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk has-perk-populated">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/saboteur.png" />
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+      </div>
+      <div class="inventory-row odd">
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+      </div>
+      <div class="inventory-row even">
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+        <div class="inventory-perk">
+          <button type="button" class="inventory-perk-btn">
+            <img class="inventory-empty-perk-img" src="https://raw.githubusercontent.com/mirceachirita/dbdbuilds/master/static/perks/empty.png" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    @import "../assets/scss/builder";
+</style>
